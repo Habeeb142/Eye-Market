6726418ec79f6f6b4e79778f0ef60139e6f70597
Mustapha Habeeb <43966404+Habeeb142@users.noreply.github.com>
2020-07-10 08:52:27 +0100

    Delete env.ts

--- src/app/env.ts
+++
@@ -1,9 +0,0 @@
-export class Env {
-    // fakeUrl = 'https://eyemarket.herokuapp.com/BDR';
-    // fakePocUrl = 'https://eyemarket.herokuapp.com/pocs';
-    fakeUrl = 'http://localhost:4000/BDR';
-    fakePocUrl = 'http://localhost:4000/pocs';
-
-    ADDRESS_API = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-    API_KEY = 'AIzaSyDRPe97l7E7xJ-jv74rKaZ-KqCWfndY1is';
-}
